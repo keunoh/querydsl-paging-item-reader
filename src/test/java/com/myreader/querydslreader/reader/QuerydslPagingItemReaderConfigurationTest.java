415e8aa953c6125b80616147d01665286a829595
keunoh <rmsdhcjswo1@gmail.com>
2022-12-21 13:16:33 +0900

    Test for Spring Batch Job that I registered

--- src/test/java/com/myreader/querydslreader/reader/QuerydslPagingItemReaderConfigurationTest.java
+++ src/test/java/com/myreader/querydslreader/reader/QuerydslPagingItemReaderConfigurationTest.java
@@ -2,25 +2,20 @@ package com.myreader.querydslreader.reader;
 
 
 import com.myreader.querydslreader.QuerydslPagingItemReaderConfiguration;
-import com.myreader.querydslreader.QuerydslReaderApplication;
 import com.myreader.querydslreader.QuerydslReaderApplicationTests;
 import com.myreader.querydslreader.setting.entity.Manufacture;
 import com.myreader.querydslreader.setting.entity.ManufactureBackup;
 import com.myreader.querydslreader.setting.entity.ManufactureBackupRepository;
 import com.myreader.querydslreader.setting.entity.ManufactureRepository;
-import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.test.JobLauncherTestUtils;
 import org.springframework.batch.test.context.SpringBatchTest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
