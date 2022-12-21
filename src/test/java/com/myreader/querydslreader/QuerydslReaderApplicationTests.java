cd4e110565b7d1d48b4a9e0116f3504abc4638fd
keunoh <rmsdhcjswo1@gmail.com>
2022-12-21 11:53:21 +0900

    스프링 배치 테스트를 위한 애노테이션 추가

---
+++ src/test/java/com/myreader/querydslreader/QuerydslReaderApplicationTests.java
@@ -0,0 +1,17 @@
+package com.myreader.querydslreader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@EnableBatchProcessing
+@EnableAutoConfiguration
+public class QuerydslReaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
