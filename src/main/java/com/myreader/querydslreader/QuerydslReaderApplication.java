c37d6858925f968d2d775ab0c6d044af8b6a3745
keunoh <rmsdhcjswo1@gmail.com>
2022-12-21 11:49:11 +0900

    querydsl reader

---
+++ src/main/java/com/myreader/querydslreader/QuerydslReaderApplication.java
@@ -0,0 +1,13 @@
+package com.myreader.querydslreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuerydslReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuerydslReaderApplication.class, args);
+	}
+
+}
